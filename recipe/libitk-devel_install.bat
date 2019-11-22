58b3fe0fa6044d0444a9ba2fd1992d924d3bca2a
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-11-22 17:10:45 -0500

    Split out a development sub-package  with the headers

---
+++ recipe/libitk-devel_install.bat
@@ -0,0 +1,4 @@
+set BUILD_DIR=%SRC_DIR%\bld
+cmake -DCOMPONENT=Development -P %BUILD_DIR%\cmake_install.cmake
+
+
---
+++ recipe/libitk-devel_install.sh
@@ -0,0 +1,3 @@
+BUILD_DIR=${SRC_DIR}/build
+cmake -DCOMPONENT=Development -P ${BUILD_DIR}/cmake_install.cmake
+
