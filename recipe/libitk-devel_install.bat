d164b336b8e1869dd9b7ad0339c4986f47a90e09
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-11-23 09:01:56 -0500

    Install DebugDevel for GDCM ARCHIVE components

--- recipe/libitk-devel_install.bat
+++ recipe/libitk-devel_install.bat
@@ -1,4 +1,6 @@
 set BUILD_DIR=%SRC_DIR%\bld
 cmake -DCOMPONENT=Development -P %BUILD_DIR%\cmake_install.cmake
+cmake -DCOMPONENT=DebugDevel -P %BUILD_DIR%\cmake_install.cmake
+cmake -DCOMPONENT=cpplibraries  -P %BUILD_DIR%\cmake_install.cmake
 
 
