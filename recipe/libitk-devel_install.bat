48a90b1648900709b0b923dec174152d986be9ae
Bradley Lowekamp <blowekamp@mail.nih.gov>
2020-04-14 11:27:11 -0400

    Include GDCM header files in libitk-devel package
    Add missing Headers cmake install component used by GDCM.

--- recipe/libitk-devel_install.bat
+++ recipe/libitk-devel_install.bat
@@ -2,5 +2,5 @@ set BUILD_DIR=%SRC_DIR%\bld
 cmake -DCOMPONENT=Development -P %BUILD_DIR%\cmake_install.cmake
 cmake -DCOMPONENT=DebugDevel -P %BUILD_DIR%\cmake_install.cmake
 cmake -DCOMPONENT=cpplibraries  -P %BUILD_DIR%\cmake_install.cmake
-
+cmake -DCOMPONENT=Headers -P %BUILD_DIR%\cmake_install.cmake
 
